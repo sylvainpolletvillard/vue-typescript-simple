3bc49de0009b8832153eb834e1889400d758cc9c
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2017-06-07 00:03:54 +0200

    add vue-expose-component-names

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import VueExposeComponentNames from "vue-expose-component-names"
+
 import App from './components/App.vue'
 
+Vue.use(VueExposeComponentNames);
+
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+	el: '#app',
+	render: h => h(App)
+});
